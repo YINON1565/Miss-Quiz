e59c1f07904b136b3e5da89796d9ba20e85b1b4e
Yinon_Eliyahu <yinoneliyaoo@gmail.com>
2020-09-25 01:58:32 +0300

    change name for miss-quiz

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('online-test app is running!');
+    expect(page.getTitleText()).toEqual('miss-quiz app is running!');
   });
 
   afterEach(async () => {
