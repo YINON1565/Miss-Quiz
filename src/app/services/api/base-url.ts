8a1e8f2563da58a25bd4ea4d05333130e34057b3
Yinon_Eliyahu <yinoneliyaoo@gmail.com>
2020-10-02 13:08:35 +0300

    environment.production

--- src/app/services/api/base-url.ts
+++ src/app/services/api/base-url.ts
@@ -1,3 +1,4 @@
 import { environment } from '../../../environments/environment';
+console.log(environment.production, 'environment.production');
 
 export const BASE_URL = environment.production ? '/api/' : '//localhost:3030/api/' ;
